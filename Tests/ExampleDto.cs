8c6930880a6a42cb24babf0f50ffb7207f867ede
Henry <emorell96@hotmail.com>
2022-08-20 02:11:57 +0200

    dto example

---
+++ Tests/ExampleDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class ExampleDto
+    {
+        public string? Name { get; set; }
+        public string? Optional { get; set; }
+    }
+}
---
+++ Tests/JsonTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class JsonTests
+    {
+        public static string ExpectedJson = "{\"Name\":\"My name\"}";
+
+        [TestMethod]
+        public void TestOptionalParamater()
+        {
+            var dto = new ExampleDto
+            {
+                Name = "My name",
+                Optional = null
+            };
+
+            var json = JsonSerializer.Serialize<ExampleDto>(dto, new JsonSerializerOptions
+            {
+                DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+            });
+            json.Should().Be(ExpectedJson);
+
+            var deserializedJson = JsonSerializer.Deserialize<ExampleDto>(json);
+
+            deserializedJson.Should().BeEquivalentTo(dto);
+
+        }
+    }
+}
